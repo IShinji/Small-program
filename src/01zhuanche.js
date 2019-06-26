6f35b42fcc01e4773e3b169753ac364a16e6f0d5
邹红全 <icanghai@foxmail.com>
2019-06-26 12:27:00 +0800

    refactor: Change 01zhuanche time to 5s

--- src/01zhuanche.js
+++ src/01zhuanche.js
@@ -2,4 +2,4 @@
 setInterval(() => {
     document.querySelector('.btn_go') && document.querySelector('.btn_go').click();
     document.querySelector('.back_index') && document.querySelector('.back_index').click();
-}, 1000);
+}, 5000);
